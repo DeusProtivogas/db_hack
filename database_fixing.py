7fb122a31f260354002a05d4a24da6ba63fe9970
DeusProtivogas <kirill2689@gmail.com>
2023-12-26 13:00:24 +0300

    Add files via upload

--- database_fixing.py
+++ database_fixing.py
@@ -1,45 +1,85 @@
+import random
+
 from datacenter.models import Mark
 from datacenter.models import Schoolkid
 from datacenter.models import Chastisement
 from datacenter.models import Commendation
 from datacenter.models import Subject
 from datacenter.models import Lesson
+from django.core.exceptions import MultipleObjectsReturned
+from django.core.exceptions import ObjectDoesNotExist
+
+COMMENDATIONS = ["Хвалю!"]
 
 
 def fix_marks(schoolkid):
-    student = Schoolkid.objects.get(full_name__contains=schoolkid)
-    Mark.objects.filter(
-        points__lt=4,
-        schoolkid=student,
-    ).update(points=5)
+    try:
+        student = Schoolkid.objects.get(full_name__contains=schoolkid)
+        Mark.objects.filter(
+            points__lt=4,
+            schoolkid=student,
+        ).update(points=5)
+    except MultipleObjectsReturned:
+        print("Имя недостаточно однозначно")
+    except ObjectDoesNotExist:
+        print("Студент не найден!")
 
 
 def remove_chastisements(schoolkid):
-    student = Schoolkid.objects.get(full_name__contains=schoolkid)
-    Chastisement.objects.filter(
-        schoolkid=student,
-    ).delete()
+    try:
+        student = Schoolkid.objects.get(full_name__contains=schoolkid)
+        Chastisement.objects.filter(
+            schoolkid=student,
+        ).delete()
+    except MultipleObjectsReturned:
+        print("Имя недостаточно однозначно")
+    except ObjectDoesNotExist:
+        print("Студент не найден!")
 
 
 def create_commendation(schoolkid_name, subject_name):
-    student = Schoolkid.objects.get(full_name__contains=schoolkid_name)
-    subject = Subject.objects.get(
-        title__contains=subject_name,
-        year_of_study=student.year_of_study
-    )
-    date = Lesson.objects.filter(
+    try:
+        student = Schoolkid.objects.get(full_name__contains=schoolkid_name)
+    except MultipleObjectsReturned:
+        print("Имя недостаточно однозначно")
+        return
+    except ObjectDoesNotExist:
+        print("Студент не найден!")
+        return
+    try:
+        subject = Subject.objects.get(
+            title__contains=subject_name,
+            year_of_study=student.year_of_study
+        )
+    except MultipleObjectsReturned:
+        print("Предмет недостаточно однозначный")
+        return
+    except ObjectDoesNotExist:
+        print("Предмет не найден!")
+        return
+
+    date_query = Lesson.objects.filter(
+        year_of_study=student.year_of_study,
+        group_letter=student.group_letter,
+        subject__title=subject_name,
+    ).order_by('date').last()
+    if not date_query:
+        print("Урок не найден!")
+        return
+    date = date_query.date
+
+    teacher_query = Lesson.objects.filter(
         year_of_study=student.year_of_study,
         group_letter=student.group_letter,
         subject__title=subject_name
-    ).last().date
-    teacher = Lesson.objects.filter(
-        year_of_study=student.year_of_study,
-        group_letter=student.group_letter,
-        subject__title=subject_name
-    ).last().teacher
+    ).last()
+    if not teacher_query:
+        print("Преподаватель не найден!")
+        return
+    teacher = date_query.teacher
 
     Commendation.objects.create(
-        text="Хвалю!",
+        text=random.choice(COMMENDATIONS),
         created=date,
         schoolkid=student,
         subject=subject,
